761bf740cc2b6d53283872158373ed620767dd6c
Efrain Hernandez <efrainhg2050@gmail.com>
2022-03-12 15:35:07 -0600

    First schema for weather config function

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,5 +1,14 @@
 import weather from "../data/current-weather.js";
 
+function configCurrentWeather(weather) {
+    //loader
+    //current date
+    //city
+    //temp
+    //bg need to change like a weather
+}
+
 export default function currentWeather() {
+    configCurrentWeather(weather);
     console.log(weather);
 }
