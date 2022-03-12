aa18a147399ba1533ea05b5912cda0534c9398fa
Efrain Hernandez <efrainhg2050@gmail.com>
2022-03-12 15:56:41 -0600

    setCurrent city function for location settings

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,11 +1,16 @@
 import weather from "../data/current-weather.js";
 
+function setCurrentCity($element, city) {
+    $element.textContent = city;
+}
+
 function configCurrentWeather(weather) {
     //loader
     //current date
     //city
     const $currentWeatherCity = document.querySelector('#current-weather-city');
-    debugger
+    const city = weather.name;
+    setCurrentCity($currentWeatherCity, city);
     //temp
     //bg need to change like a weather
 }
