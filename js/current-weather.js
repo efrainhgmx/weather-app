199f5593f89788ee6134f5b2f6fdd28090c0666a
Efrain Hernandez <efrainhg2050@gmail.com>
2022-03-14 22:41:01 -0600

    Intl for date format

--- js/current-weather.js
+++ js/current-weather.js
@@ -4,6 +4,18 @@ function setCurrentCity($element, city) {
     $element.textContent = city;
 }
 
+const config = {
+    day: 'numeric',
+    weekday: 'long',
+    month: 'long'
+}
+
+function setCurrentDate($element) {
+    const date = new Date();
+    const formattedDate = new Intl.DateTimeFormat('es', config).format(date);
+    $element.textContent = formattedDate;
+}
+
 function configCurrentWeather(weather) {
     //loader
     //current date
