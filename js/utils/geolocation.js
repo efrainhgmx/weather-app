3f90a887bec5f8a77f39cc02df544c3a723f20e9
Efrain Hernandez <efrainhg2050@gmail.com>
2022-03-22 23:35:38 -0600

    current position using a function

--- js/utils/geolocation.js
+++ js/utils/geolocation.js
@@ -1,3 +1,9 @@
 function geolocationSupport() {
     return 'geolocation' in navigator;
+};
+
+function getCurrentPosition() {
+    if(!geolocationSupport()) throw new Error('No hay soporte de geolocalizacion en tu navegador');
+
+    navigator.geolocation.getCurrentPosition()
 }
