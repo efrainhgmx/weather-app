f32facd18f1fd9f290482a0a93ee26292daa3c9f
Efrain Hernandez <efrainhg2050@gmail.com>
2022-03-28 20:08:47 -0600

    Chaging Farenheit to Celcius for the temperature

--- js/services/weather.js
+++ js/services/weather.js
@@ -1,6 +1,6 @@
 import { API_KEY, BASE_API } from "../constants.js";
 export async function getCurrentWeather(lat, lon) {
-    const response = await fetch(`${BASE_API}weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    const response = await fetch(`${BASE_API}weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
     if(!response.ok) return {
         isError: true,
         data: null
