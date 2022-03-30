855fe4e0acdca2a79134bcfa6d9b2cb5196e235e
Efrain Hernandez <efrainhg2050@gmail.com>
2022-03-30 16:47:13 -0600

    Dinamyc days function

--- js/tabs.js
+++ js/tabs.js
@@ -3,20 +3,28 @@ const $tabList = $tabsCotainer.querySelectorAll('.tab');
 const today = new Date();
 let weekday = today.getDay();
 
-const week = ['Domingo',
+const week = [
+ 'Domingo',
  'Lunes', 
  'Martes', 
  'Miércoles', 
  'Jueves', 
- 'Viernes'
+ 'Viernes',
+ 'Sábado'
 ];
 
 function nextDay(day) {
-    return day + 1;
+    return (day === 6 ) ? 0 : day + 1;
 }
 
 
 $tabList.forEach(($tab, index) => {
+    console.log(index);
+    if(index === 0) {
+        $tab.textContent = 'Hoy';
+        weekday = nextDay(weekday);
+        return;
+    }
     $tab.textContent = `${week[weekday]}`;
     weekday = nextDay(weekday);
 })
