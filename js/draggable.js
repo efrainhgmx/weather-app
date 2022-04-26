893c52607fff885fd4893e3608fd77ae64d08b06
Efrain Hernandez <efrainhg2050@gmail.com>
2022-04-25 23:16:17 -0500

    Debug mode in draggable module

--- js/draggable.js
+++ js/draggable.js
@@ -9,9 +9,32 @@ export default function draggable($element, config = defaultConfig) {
         return console.warn(`Elemento invalido ${$element}, se espera un elemento HTML`);
     }
 
+    let isOpen = config,open;
+
+    isOpen ? open() : close();
+
+
+    const VISIBLE_Y_POSITION = 0;
+
+    const HIDDEN_Y_POSITION = 0;
+    
     function logger(message) {
         if(config.debug) {
             console.info(message);
         }
+    }
+
+    function open() {
+        logger('Abrir widget');
+        isOpen = true;
+    }
+
+    function close() {
+        logger('Cerrar widget')
+        isOpen = false;
+    }
+
+    function setWidgetPosition(value) {
+        $element.style.marginBottom = `-${value}px`;
     }
 }
