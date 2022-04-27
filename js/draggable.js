f5fccca1a84660ac4492a682b1c1ffc326bc58fe
Efrain Hernandez <efrainhg2050@gmail.com>
2022-04-26 22:53:06 -0500

    start and drag event

--- js/draggable.js
+++ js/draggable.js
@@ -19,9 +19,11 @@ export default function draggable($element, config = defaultConfig) {
     const VISIBLE_Y_POSITION = 0;
     const HIDDEN_Y_POSITION = ELEMENT_BLOCK_SIZE - MARKER_BLOCK_SIZE;
     
-    let widetPosition = VISIBLE_Y_POSITION;
+    let widgetPosition = VISIBLE_Y_POSITION;
     
     isOpen ? open() : close();
+
+    let startY = 0;
 
     $marker.addEventListener('click', handleClick);
     $marker.addEventListener('pointerdown', handlePointerDown);
@@ -30,8 +32,9 @@ export default function draggable($element, config = defaultConfig) {
     $marker.addEventListener('pointercancel', handlePointerCancel);
     $marker.addEventListener('pointermove', handlePointerMove);
 
-    function handlePointerMove() {
-        logger('Pointer move')
+    function handlePointerMove(event) {
+        logger('Pointer move');
+        drag(event)
     }
 
     function handlePointerCancel() {
@@ -42,8 +45,9 @@ export default function draggable($element, config = defaultConfig) {
         logger('Pointer out')
     }
 
-    function handlePointerDown() {
+    function handlePointerDown(event) {
         logger('Pointer down')
+        startDrag(event);
     }
 
     function handlePointerUp() {
@@ -54,6 +58,15 @@ export default function draggable($element, config = defaultConfig) {
         logger('click');
         toggle(event)
     };
+
+    function pageY(event) {
+        return event.pageY || event.touches[0].pageY
+    }
+
+    function startDrag(event) {
+        isDragging = true;
+        startY = pageY(event);
+    }
 
     function toggle(event) {
         if(!isDragging) {
@@ -74,7 +87,7 @@ export default function draggable($element, config = defaultConfig) {
     function open() {
         logger('Abrir widget');
         isOpen = true;
-        widetPosition = VISIBLE_Y_POSITION;
+        widgetPosition = VISIBLE_Y_POSITION;
         setWidgetPosition(VISIBLE_Y_POSITION);
     }
 
@@ -86,5 +99,11 @@ export default function draggable($element, config = defaultConfig) {
 
     function setWidgetPosition(value) {
         $element.style.marginBottom = `-${value}px`;
+    }
+
+    function drag(event) {
+        const cursorY = pageY(event);
+        const movementY = cursorY - startY;
+        logger(movementY);
     }
 }
