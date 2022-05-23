2ac5d03814e0525b211ec99156b276857199321e
Efrain Hernandez <efrainhg2050@gmail.com>
2022-05-22 21:40:41 -0500

    First item selected when you change the tab

--- js/tabs.js
+++ js/tabs.js
@@ -19,6 +19,11 @@ function nextDay(day) {
     return (day === 6 ) ? 0 : day + 1;
 }
 
+function newDaySelected(index) {
+    const firstItemOfDay = document.querySelectorAll('.dayWeather-item');
+    firstItemOfDay[index * 8].classList.add('is-selected');
+}
+
 function handleSelectTabClick(event) {
     const $tabSelected = event.target;
     const $tabActive = document.querySelector('.tab[aria-selected="true"');
@@ -33,7 +38,7 @@ function handleSelectTabClick(event) {
     $tabPanelSelected.hidden = true;
     console.log(id);
     dayWeatherSummary(defaultWeather, tabRefNumber)
-    
+    newDaySelected(tabRefNumber);
 }
 
 $tabList.forEach(($tab, index) => {
